6aa6143606b35e588b74de6b24be897d2de124de
shekhar <shekhar@excellencetechnologies.in>
2017-01-25 11:18:10 +0530

    fixed

--- routes/v2/product.js
+++ routes/v2/product.js
@@ -79,8 +79,8 @@ router.all('/view', function (req, res, next) {
     } else {
         var body = req.body;
         var productObj = req.productObj;
-        // var product_id = body.product_id;
-        var product_id = '58834c514e25e2e9058b4567'; //for testing
+        var product_id = body.product_id;
+        // var product_id = '58834c514e25e2e9058b4567'; //for testing
         var category = req.conn_category;
         var website_scrap_data = req.conn_website_scrap_data;
         if (typeof product_id === 'undefined') {
