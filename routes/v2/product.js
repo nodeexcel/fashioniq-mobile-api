83c53937a8d262ab12f9302691d957091af67b4e
shekhar <shekhar@excellencetechnologies.in>
2017-01-31 18:59:42 +0530

    product/unlike done

--- routes/v2/product.js
+++ routes/v2/product.js
@@ -316,4 +316,21 @@ router.post('/like', function (req, res) {
     }
 });
 
+router.post('/unlike', function (req, res) {
+    var body = req.body;
+    var product_id = body.product_id;
+    var website_scrap_data = req.conn_website_scrap_data;
+    if (product_id) {
+        website_scrap_data.findOneAndUpdate({_id: product_id}, {$inc: {count_likes: -1}}, function (err, product1) {
+            if (err) {
+                res.json({error: 1, message: err, data: []});
+            } else {
+                res.json({error: 0, message: 'success', data: {count_likes: product1.toJSON().count_likes}});
+            }
+        });
+    } else {
+        res.json({error: 1, message: 'product_id cannot be empty', data: []});
+    }
+});
+
 module.exports = router;
