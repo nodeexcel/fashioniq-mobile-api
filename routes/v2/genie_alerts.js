f70dbdaac5c81fa754e39c25b0e3ad34f6edf157
shekhar <shekhar@excellencetechnologies.in>
2017-03-02 17:49:09 +0530

    fix alerts iisue

--- routes/v2/genie_alerts.js
+++ routes/v2/genie_alerts.js
@@ -64,7 +64,7 @@ router.all('/set_genie_alert', function(req, res, next) {
                                             } else {
                                                 var push_token = resp.get('token');
                                             }
-                                            var payload = { product_id: exist_product._id };
+                                            var payload = { product_id: product._id };
                                             var notify = {
                                                 title: 'Price alert subscribed',
                                                 body: 'Price alert subscribed for ' + Product_name,
