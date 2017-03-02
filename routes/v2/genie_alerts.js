54f10bc0e8ddd9ed61066f2fc3221f5ad4f8d003
shekhar <shekhar@excellencetechnologies.in>
2017-03-02 17:19:07 +0530

    send push notification when genie_alert_subscribed or unsubscribed

--- routes/v2/genie_alerts.js
+++ routes/v2/genie_alerts.js
@@ -11,7 +11,8 @@ router.all('/set_genie_alert', function(req, res, next) {
     var mongo_id = body.mongo_id;
     var email_id = body.email_id;
     var website_scrap_data = req.conn_website_scrap_data;
-
+    var UserModel = req.User;
+    var log_push_notification = req.log_push_notification;
     var where = {
         _id: mongoose.Types.ObjectId(mongo_id)
     };
@@ -24,6 +25,7 @@ router.all('/set_genie_alert', function(req, res, next) {
             } else {
                 product = product[0];
                 var email = product.get('genie_alerts');
+                var Product_name = product.get('name');
                 var check_email = _.some(email, { "email_id": email_id });
                 if (check_email == false) {
                     if (product.get('genie_alerts')) {
@@ -43,6 +45,66 @@ router.all('/set_genie_alert', function(req, res, next) {
                         if (err) {
                             res.json({ error: 1, message: err });
                         } else {
+                            UserModel.findOne({ email: email_id }, function(err, user) {
+                                if (err) {
+                                    console.log(err);
+                                } else if (!user) {
+                                    console.log('not found');
+                                } else {
+                                    req.GCM.findOne({ user_id: user._id.toString() }, function(error, resp) {
+                                        if (error) {
+                                            console.log(err)
+                                        } else if (!resp) {
+                                            console.log('not found');
+                                        } else if (!resp.get('reg_id') && resp.get('token') == null) {
+                                            console.log('token null');
+                                        } else {
+                                            if (resp.get('reg_id')) {
+                                                var push_token = resp.get('reg_id');
+                                            } else {
+                                                var push_token = resp.get('token');
+                                            }
+                                            var payload = {};
+                                            var notify = {
+                                                title: 'Price alert subscribed',
+                                                body: 'Price alert subscribed for ' + Product_name,
+                                                click_action: "FCM_PLUGIN_ACTIVITY",
+                                                "color": "#f95b2c"
+                                            };
+                                            var serverKey = 'AAAAoDVUotg:APA91bGSSsdmlLFx9ihoi3Kq7XMrYr24pMKfL93j4M9p7qNg8eWeqYWmp9HSfbUhaqjZxEC9uvrXQ6_Fgs5mKUcov2xNKHqkKxUNx9Bd7rjhYJ2g7472Z-DxkPLZYv4cny8wah4w1LON';
+                                            PUSH_MESSAGE.push_notification(serverKey, push_token, payload, notify, function(error, response) {
+                                                if (error == 'error') {
+                                                    console.log(response);
+                                                } else {
+                                                    var parse_response = JSON.parse(response);
+                                                    if (parse_response.success == 1) {
+                                                        push_sent_status = 'sent';
+                                                    } else {
+                                                        push_sent_status = 'not sent';
+                                                    }
+                                                    var push_info = {
+                                                        push_sent_status: push_sent_status,
+                                                        push_sent_response: response,
+                                                        scrap_product_id: product._id,
+                                                        website: product.get('website'),
+                                                        time: new Date(),
+                                                        email_id: email_id,
+                                                        genie_alerts: 'subscribed'
+                                                    };
+                                                    var insert_push_info = new log_push_notification(push_info);
+                                                    insert_push_info.save(function(err, resp) {
+                                                        if (err) {
+                                                            console.log(err)
+                                                        } else {
+                                                            console.log(resp);
+                                                        }
+                                                    })
+                                                }
+                                            });
+                                        }
+                                    })
+                                }
+                            })
                             res.json({ status: 0, message: "Price alert subscribed" });
                         }
                     });
@@ -74,67 +136,76 @@ router.all('/unset_genie_alert', function(req, res, next) {
             if (err) {
                 res.json({ error: 1, message: err });
             } else if (response == 1) {
-                UserModel.findOne({ email: email }, function(err, user) {
+                website_scrap_data.find(where, function(err, result) {
                     if (err) {
                         console.log(err);
-                    } else if (!user) {
-                        console.log('not found');
+                    } else if (result.length == 0) {
+                        console.log('product not exist');
                     } else {
-                        req.GCM.findOne({ user_id: user._id.toString() }, function(error, resp) {
-                            if (error) {
-                                console.log(err)
-                            } else if (!resp) {
+                        exist_product = result[0];
+                        var Product_name = exist_product.get('name');
+                        UserModel.findOne({ email: email_id }, function(err, user) {
+                            if (err) {
+                                console.log(err);
+                            } else if (!user) {
                                 console.log('not found');
-                            } else if (!resp.get('reg_id') && resp.get('token') == null) {
-                                console.log('token null');
                             } else {
-                                if (resp.get('reg_id')) {
-                                    var push_token = resp.get('reg_id');
-                                } else {
-                                    var push_token = resp.get('token');
-                                }
-                                var payload = {};
-                                var notify = {
-                                    title: 'Price down alert',
-                                    body: 'Price alert unsubscribed for' + 'Product name',
-                                    click_action: "FCM_PLUGIN_ACTIVITY",
-                                    "color": "#f95b2c"
-                                };
-                                var serverKey = 'AAAAoDVUotg:APA91bGSSsdmlLFx9ihoi3Kq7XMrYr24pMKfL93j4M9p7qNg8eWeqYWmp9HSfbUhaqjZxEC9uvrXQ6_Fgs5mKUcov2xNKHqkKxUNx9Bd7rjhYJ2g7472Z-DxkPLZYv4cny8wah4w1LON';
-                                PUSH_MESSAGE.push_notification(serverKey, push_token, payload, notify, function(error, response) {
-                                    if (error == 'error') {
-                                        console.log(response);
+                                req.GCM.findOne({ user_id: user._id.toString() }, function(error, resp) {
+                                    if (error) {
+                                        console.log(err)
+                                    } else if (!resp) {
+                                        console.log('not found');
+                                    } else if (!resp.get('reg_id') && resp.get('token') == null) {
+                                        console.log('token null');
                                     } else {
-                                        var parse_response = JSON.parse(response);
-                                        if (parse_response.success == 1) {
-                                            push_sent_status = 'sent';
+                                        if (resp.get('reg_id')) {
+                                            var push_token = resp.get('reg_id');
                                         } else {
-                                            push_sent_status = 'not sent';
+                                            var push_token = resp.get('token');
                                         }
-                                        var push_info = {
-                                            push_sent_status: push_sent_status,
-                                            push_sent_response: response,
-                                            // scrap_product_id: exist_product._id,
-                                            // website: exist_product.get('website'),
-                                            time: PARSER.currentIsoDate(),
-                                            email_id: email_id,
-                                            old_price: exist_product_price,
-                                            new_price: new_data_price
+                                        var payload = {};
+                                        var notify = {
+                                            title: 'Price alert unsubscribed',
+                                            body: 'Price alert unsubscribed for ' + Product_name,
+                                            click_action: "FCM_PLUGIN_ACTIVITY",
+                                            "color": "#f95b2c"
                                         };
-                                        var insert_push_info = new log_push_notification(push_info);
-                                        insert_push_info.save(function(err, resp) {
-                                            if (err) {
-                                                console.log(err)
+                                        var serverKey = 'AAAAoDVUotg:APA91bGSSsdmlLFx9ihoi3Kq7XMrYr24pMKfL93j4M9p7qNg8eWeqYWmp9HSfbUhaqjZxEC9uvrXQ6_Fgs5mKUcov2xNKHqkKxUNx9Bd7rjhYJ2g7472Z-DxkPLZYv4cny8wah4w1LON';
+                                        PUSH_MESSAGE.push_notification(serverKey, push_token, payload, notify, function(error, response) {
+                                            if (error == 'error') {
+                                                console.log(response);
                                             } else {
-                                                console.log(resp);
+                                                var parse_response = JSON.parse(response);
+                                                if (parse_response.success == 1) {
+                                                    push_sent_status = 'sent';
+                                                } else {
+                                                    push_sent_status = 'not sent';
+                                                }
+                                                var push_info = {
+                                                    push_sent_status: push_sent_status,
+                                                    push_sent_response: response,
+                                                    scrap_product_id: exist_product._id,
+                                                    website: exist_product.get('website'),
+                                                    time: new Date(),
+                                                    email_id: email_id,
+                                                    genie_alerts: 'unsubscribed'
+                                                };
+                                                var insert_push_info = new log_push_notification(push_info);
+                                                insert_push_info.save(function(err, resp) {
+                                                    if (err) {
+                                                        console.log(err)
+                                                    } else {
+                                                        console.log(resp);
+                                                    }
+                                                })
                                             }
-                                        })
+                                        });
                                     }
-                                });
+                                })
                             }
                         })
                     }
-                })
+                });
                 res.json({ status: 0, message: "Price alert unsubscribed" });
             } else {
                 res.json({ status: 0, message: "product not found" });
